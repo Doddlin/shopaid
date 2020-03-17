da81c75a64be2685df738d10f102a0a05ee66df1
Didrik Birkemalm <didrik@dbirkemalm.com>
2020-03-17 12:48:18 +0100

    Testing map

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :country, :city, :postcode, :ishelper])
+        devise_parameter_sanitizer.permit([:sign_up, :account_update], keys: [:name, :email, :password, :country, :city, :postcode, :ishelper])
       end
 
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
 
     def index
         @helpers = User.where(ishelper: true).count
-        @allhelpers = User.where(ishelper: true)
+        @allhelpers = User.where.not(lat:nil)
+
         @markers = @allhelpers.map do |helper|
             {
                 latlng: [helper[:lat], helper[:long]],
