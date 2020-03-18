be2ad7e107dc188f4332626a82a14d3c86eb08f2
Didrik Birkemalm <didrik@dbirkemalm.com>
2020-03-18 17:34:13 +0100

    Revert "User details need permission"
    This reverts commit bdcc966c533028ad3c2d7dcccf7e5beaf9816714.

--- app/mailers/notify_near_list_mailer.rb
+++
@@ -1,7 +0,0 @@
-class NotifyNearListMailer < ApplicationMailer
-    def welcome_email
-        @user = params[:user]
-        @url  = 'http://example.com/login'
-        mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-      end
-end
--- test/mailers/notify_near_list_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class NotifyNearListMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
