bdcc966c533028ad3c2d7dcccf7e5beaf9816714
Didrik Birkemalm <didrik@dbirkemalm.com>
2020-03-18 17:34:13 +0100

    User details need permission

---
+++ test/mailers/notify_near_list_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NotifyNearListMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/notify_near_list_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/notify_near_list_mailer
+class NotifyNearListMailerPreview < ActionMailer::Preview
+
+end
