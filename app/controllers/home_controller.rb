1922596a5865bb429e704952f62c7b21933d4c78
Didrik Birkemalm <didrik@dbirkemalm.com>
2020-03-23 12:26:51 +0100

    Changes made

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,13 @@ class HomeController < ApplicationController
     def index
         @helpers = User.where(ishelper: true).count
         @allhelpers = User.where.not(lat:nil)
+        @circles = [max: 20, min: 5]
+
 
         @markers = @allhelpers.map do |helper|
             {
-                latlng: [helper[:lat], helper[:long]],
+                latlng: [helper[:lat], helper[:long]], radius: 300,
+                color: "#333333", fillColor: "#444444", fillOpacity: 1
             }
         end
     end
