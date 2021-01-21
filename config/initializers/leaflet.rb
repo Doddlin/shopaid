efdd2daaa45873930918d29e8f0f4d91c304ff51
Didrik Birkemalm <didrik.birkemalm@gmail.com>
2021-01-21 15:06:22 +0100

    leafletupdated2

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,3 +1,2 @@
 Leaflet.tile_layer = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic250Y2EiLCJhIjoiY2s3dXd0ZGRoMTNqYTNmc2Z5emh5cnY1YyJ9.A8XuFX1GTp1W982abVbpCg'
 Leaflet.max_zoom = 25
-Leaflet.id = 'mapbox/streets-v11'
